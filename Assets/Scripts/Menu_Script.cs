ac2e04d2b7afe0c0dfd11b7c1e22938c79002520
Nick Ochsner <nochsner@uncc.edu>
2025-08-08 22:39:34 -0400

    Finished both levels

--- Assets/Scripts/Menu_Script.cs
+++ Assets/Scripts/Menu_Script.cs
@@ -4,9 +4,14 @@ using UnityEngine.SceneManagement;
 public class Menu_Script : MonoBehaviour
 {
 
-    public void PlayGame()
+    public void PlayGame_L1()
     {
         print("Loading Level 1");
         SceneManager.LoadScene("Level_1");
+    }
+    public void PlayGame_L2()
+    {
+        print("Loading Level 2");
+        SceneManager.LoadScene("Level_2");
     }
 }
